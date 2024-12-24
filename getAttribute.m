086d9984db7c9ecb27797c6f78fcc3665f2cf034
Komasa Qi <komasaqi@foxmail.com>
2024-12-24 10:48:16 +0800

    Delete getAttribute.m

--- getAttribute.m
+++
@@ -1,9 +0,0 @@
-function value = getAttribute(body, attribute)
-    value = [];
-    for i = 1:length(body.Attributes)
-        if strcmp(body.Attributes(i).Name,attribute)
-            value = body.Attributes(i).Value;
-            break;
-        end
-    end
-end
