36df50380a6a16daca491329c3276574cb9a57da
Komasa Qi <komasaqi@foxmail.com>
2024-12-24 10:49:24 +0800

    Delete getEntityInRange.m

--- getEntityInRange.m
+++
@@ -1,5 +0,0 @@
-% 相比于queryEntityIDwithinRadius()，多了edgeID这个考虑
-function [edgeList, junctionList] = getEntityInRange(proxyMat,point,radius,edgeID)
-    [edgeList,junctionList] = queryEntityIDwithinRadius(proxyMat,point,radius);
-    edgeList = unique([edgeList,{edgeID}]);
-end
